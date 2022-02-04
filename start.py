3112df1377358b67bb20acb8912fcd9740cfcd66
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-02-04 22:39:10 +0900

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,12 @@
 import os # Imports the "os" library
 import mvDecryptor # Imports "mvDecryptor.py"
 
-if os.path.exists(os.getcwd()+"/www/data/System.json"): # If they click OK, check if System.json exists.
+dir = os.getcwd()
+
+if os.path.exists(dir+"/www/data/System.json"): # If they click OK, check if System.json exists.
     mvDecryptor.decryptEntireGame(dir) # If so, decrypt the game.
     print("success")
 else: # If it is not found
     pass
-    print(os.getcwd()+"/www/data/System.json not found.")
+    print(dir+"/www/data/System.json not found.")
 os._exit(0) # Exit the application (avoids memory leaks)
